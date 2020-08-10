814d6c1a26e776cd3d2a8b55a163223a27ba9058
marlongd <na>
2020-08-10 12:14:40 -0600

    Learning route config

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -12,6 +12,11 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movie", action = "ByReleaseDate"}
+            );
 
             routes.MapRoute(
                 name: "Default",
---
+++ Vidly/Vidly/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+            //return Content("HELLO WORLD");
+            //return RedirectToAction("Index");
+            
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year +"/" + month);
+        }
+    }
+
+    
+}
