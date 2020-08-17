8244cf0c3ee0ab5a20ffe83630f96b60a98ff369
Marlon H <nolramgd@gmail.com>
2020-08-17 17:25:01 -0600

    Stopped at view Models

--- Vidly_authenticate/Vidly_authenticate/Controllers/MoviesController.cs
+++ Vidly_authenticate/Vidly_authenticate/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.MappingViews;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,6 +16,24 @@ namespace Vidly_authenticate.Controllers
             var movie = new Movie() { Name = "Shrek!" };
             //return View(movie);
             return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id:" + id);
+        }
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue) { pageIndex = 1;  }
+            if (String.IsNullOrWhiteSpace(sortBy)) { sortBy = "Name"; }
+
+            return Content(String.Format("pageIndex ={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4})}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
