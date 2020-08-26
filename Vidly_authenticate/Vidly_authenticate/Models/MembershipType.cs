170e3dca99f63dd44420ed9d89eb000d14149b10
Marlon H <nolramgd@gmail.com>
2020-08-26 16:05:51 -0600

    Starting querying objects

---
+++ Vidly_authenticate/Vidly_authenticate/Models/MembershipType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly_authenticate.Models
+{
+    public class MembershipType
+    {
+        public int SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+        public byte Id { get; set; }
+    }
+}
