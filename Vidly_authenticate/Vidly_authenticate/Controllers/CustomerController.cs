42594d67d87b72ba0bde0016aadab56c36c5c0e1
marlongd <na>
2020-08-26 08:11:04 -0600

    Doing the exercise in part 2. trying to get customers list done

---
+++ Vidly_authenticate/Vidly_authenticate/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly_authenticate.Models;
+using Vidly_authenticate.ViewModels;
+
+namespace Vidly_authenticate.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var Customers = getCustomers();
+
+            return View(Customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            string name = "WHAT";
+            return Content("id:" + name);
+        }
+
+        private IEnumerable<Customer> getCustomers()
+        {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            return customers;
+        }
+    }
+}
