2b59271f006dbe64f71e7f72a23027eb01170d89
Marlon <nolramgd@gmail.com>
2020-12-23 11:32:29 -0700

    Skeleton for DatingApp api

---
+++ DatingApp/DatingAppApi/DatingAppApi/Controllers/ValueController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DatingAppApi.Data;
+using DatingAppApi.Models;
+
+namespace DatingAppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValueController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Value
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET: api/Value/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+
+            return Ok(value);
+        }
+
+        // PUT: api/Value/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutValue(int id, Value value)
+        {
+            if (id != value.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(value).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ValueExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Value
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Value>> PostValue(Value value)
+        {
+            _context.Values.Add(value);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetValue", new { id = value.Id }, value);
+        }
+
+        // DELETE: api/Value/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Value>> DeleteValue(int id)
+        {
+            var value = await _context.Values.FindAsync(id);
+            if (value == null)
+            {
+                return NotFound();
+            }
+
+            _context.Values.Remove(value);
+            await _context.SaveChangesAsync();
+
+            return value;
+        }
+
+        private bool ValueExists(int id)
+        {
+            return _context.Values.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DatingApp/DatingAppApi/DatingAppApi/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingAppApi.Models;
+using Microsoft.EntityFrameworkCore;
+namespace DatingAppApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
